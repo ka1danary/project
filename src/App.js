c2b7d6362e6820325ceae0e67d984585b56a63db
Potega Xenia <goldenksenia145@gmail.com>
2023-10-09 12:29:19 +1000

    c

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import Header from "./components/header/Header";
+import { Route, Routes} from 'react-router-dom'
+import "./App.sass"
+import "./reset.sass"
+import Home from "./pages/home/Home";
+import Download from "./pages/download/Download";
+import View from "./pages/view/View";
+import Experiment from "./pages/experiment/Experiment";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/download" element={<Download/>} />
+        <Route path="/view" element={<View/>}/>
+        <Route path="/experiment" element={<Experiment/>}/>
+      </Routes>
     </div>
   );
 }
